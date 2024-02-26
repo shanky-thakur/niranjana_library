4393839ac27299b4182eb1e3a9adb7d34be8a4ea
shanky <thakurdeepanshu420@gmail.com>
2024-02-26 15:23:12 +0530

    try

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,7 +15,7 @@ const Navbar = () => {
 
   return (
     <nav
-      className="navbar navbar-expand-lg bg-tertiary fixed-top"
+      className="navbar navbar-expand-lg bg-tertiary"
       style={{
         backgroundColor: "#008000",
         border: "3px solid white",
